620fbcfc94d5f093db179fb7d0d5f0b25daabe1c
StevanRufceski <stevan.rufceski@gmail.com>
2024-12-01 15:51:22 +0100

    HW 29.11.24 - 1edtn

--- Homeworks JS/29.11.2024/script.js
+++ Homeworks JS/29.11.2024/script.js
@@ -1,20 +1,29 @@
-function func1 (){
-    let feets = document.getElementById("feets").value;
+
+let convertButton=document.getElementById("convertButton");
+convertButton.addEventListener("click", feetToMetersConvertor);
+function feetToMetersConvertor (){
+    let feets = Number(document.getElementById("feets").value);
     let meters = feets * 0.3048;
     alert(feets+" feets are "+meters+" meters.");
 }
-function func2 (){
-    let squareHeight = document.getElementById("height").value;
-    let squareLenght = document.getElementById("lenght").value;
+let calculateRectangle=document.getElementById("calculateRectangle");
+calculateRectangle.addEventListener("click", rectangleAreaCalculator);
+function rectangleAreaCalculator (){
+    let squareHeight = Number(document.getElementById("height").value);
+    let squareLenght = Number(document.getElementById("lenght").value);
     let squareArea = squareHeight*squareLenght;
     alert("You entered dimensions of a rectangle with "+ squareArea + " square meters area.");
 }
-function func3 (){
-    let circleRadius = document.getElementById("radius").value;
+let calculateCircle=document.getElementById("calculateCircle");
+calculateCircle.addEventListener("click", circleAreaCalculator);
+function circleAreaCalculator (){
+    let circleRadius = Number(document.getElementById("radius").value);
     let circleArea = circleRadius*circleRadius*3.14;
     alert("You entered radius of a circle with "+ circleArea + " square meters area.");
 }
-function func4 (){
+let phonesPrice=document.getElementById("phonesPrice");
+phonesPrice.addEventListener("click", phoneTotalPriceCalculator);
+function phoneTotalPriceCalculator (){
     let numberOfPhones = 30;
     let singlePhonePrice = 119.95;
     let phoneTax = 5;
@@ -22,6 +31,8 @@ function func4 (){
     let totalPrice = numberOfPhones*singlePhonePrice*taxRate;
     alert("The total price is "+ totalPrice + " dollars.");
 }
+
+
 
 
 
