fd0a8642083d2ff21403af8d38aaf1208172335e
StevanRufceski <stevan.rufceski@gmail.com>
2024-12-14 23:31:41 +0100

    14.12.2024

---
+++ Homeworks JS/24.12.13.Objects/exercizeOne.js
@@ -0,0 +1,12 @@
+let person = {
+    type: "student",
+    academy: "Qinshift",
+    class: "programming",
+    name: "Stevan",
+    surname: "Rufceski",
+    age: 44,
+    married: true,
+    haveKids: "True. Girl&Boy",
+    loveForMusic: true,
+}
+console.log(person);
---
+++ Homeworks JS/24.12.13.Objects/exercizeTwo.js
@@ -0,0 +1,10 @@
+let person = {
+    type: "student",
+    academy: "Qinshift",
+    class: "programming",
+    name: "Zoran",
+}
+console.log(person);
+person.name = "Zoki";
+console.log("After changes made......")
+console.log(person);
