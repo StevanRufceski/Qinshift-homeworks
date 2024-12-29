ebe9f7ab7ef62d823faa027cbed32283e52d7df6
Stevan Rufceski <stevan.rufceski@gmail.com>
2024-12-29 20:23:37 +0100

    291224

--- Homeworks JS/24.12.25_JQuery/exercizeTwo.js
+++ Homeworks JS/24.12.25_JQuery/exercizeTwo.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
-    $("h1").first().text("JQuery is awesome");
-    $("h1").first().after(`<p> Some text! </p>`);
-    $("button").first().css("backgroundColor", "red");
-
+    let button = $("button");
+    button.on("click", function(){
+        $("h1").first().text("JQuery is awesome");
+        $("h1").first().after(`<p> Some text! </p>`);
+        $("button").first().css("backgroundColor", "red");
+    })
 })
 console.log("connected")
