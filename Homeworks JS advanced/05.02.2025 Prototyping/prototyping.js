1c418de2c33e0f10a927a6ed484dc8d412952872
StevanRufceski <stevan.rufceski@gmail.com>
2025-02-11 13:30:47 +0100

    110225

--- Homeworks JS advanced/05.02.2025 Prototyping/prototyping.js
+++ Homeworks JS advanced/05.02.2025 Prototyping/prototyping.js
@@ -1,164 +1,162 @@
-let allStudents = [];
-const student = {
-    firstName: ``,
-    lastName: ``,
-    age: ``,
-    academy: {},
-    currrentSubject: {},
-    completedSubjects: [],
+function Institution(location, capacity){
+    this.id = new Date()
+    this.location = location
+    this.capacity = capacity
 
-    startAcademy: this.academy,
-
-    startSubject: function(subject){
-        if (this.academy === null){
-            console.log(`ERROR. The student has not enrolled into academy or the subject is not part of student's academy.`)
+    this.validateCapacity = function(theCapacity, capacity, name){
+        if (name === undefined) {
+            name = `This institute`
+        }
+        if(theCapacity<=1) {
+            return `ERROR: ${name} is has no more capacity!`
+        } else if (theCapacity>capacity) {
+            return `${name}, currrently does not have capacity of ${theCapacity} students!`
         } else {
-            this.currrentSubject = subject
+            return `${name}, currrently has capacity for ${theCapacity} students!`
+
         }
     }
 }
-const subject = {
-    title: ``,
-    numberOfClasses: 10,
-    isElectable: undefined,
-    academy: {},
-    students: [],
+const instituteForEducation = new Institution ("Skopje", 1000);
+console.log(instituteForEducation)
+console.log(instituteForEducation.validateCapacity(0, instituteForEducation.capacity))
+console.log(instituteForEducation.validateCapacity(10, instituteForEducation.capacity))
+console.log(instituteForEducation.validateCapacity(1050, instituteForEducation.capacity))
+console.log(instituteForEducation)
 
-    overrideClasses: function(number){
+function Academy(name, subjects, students, start, end){
+    Object.setPrototypeOf(this, new Institution())
+    this.name = name
+    this.subjects = subjects
+    this.students = students
+    this.start = start
+    this.end = end
+    this.numberOfClasses = this.subjects.length*10
+
+    this.printStudents = () => {
+        return this.students;
+    }
+    this.printSubjects = () => {
+        return this.subjects;
+    }
+};
+
+const qinshift = new Academy(`Qinshift`, [`uikuikl`, `tyhyth`], [`swefew`, `wefew`], `01.09.2024`, `01.09.2025`)
+console.log(qinshift)
+console.log(qinshift.printSubjects())
+console.log(qinshift.printStudents())
+console.log(qinshift.validateCapacity(0, qinshift.capacity, qinshift.name))
+console.log(qinshift.validateCapacity(20, qinshift.capacity, qinshift.name))
+console.log(qinshift.validateCapacity(120, qinshift.capacity, qinshift.name))
+console.log(qinshift)
+
+function Course(desription, price){
+    this.id = new Date()
+    this.desription = desription
+    this.price = price
+
+    this.validatePrice = function(thePrice, name){
+        if (name === undefined){
+            name = `this course`
+        }
+        if(thePrice<0) {
+            return `ERROR: The price of ${name} can't be negative!`
+        } else {
+            return `The price of ${name} course is ${thePrice}.`
+        }
+    }
+}
+const programming = new Course ("Full stack programming course", 500);
+console.log(programming)
+console.log(programming.validatePrice(5))
+console.log(programming.validatePrice(-5))
+
+function Subject(title, numberOfClasses, isElectable, academy, students){
+    Object.setPrototypeOf(this, new Course())
+    this.title = title
+    this.numberOfClasses = numberOfClasses
+    this.isElectable = isElectable
+    this.academy = academy
+    this.students = students
+
+    this.overrideClasses = function(number, name){
+        if (name === undefined) {
+            name = `ergerg`
+        }
         if (number<3) {
-            return console.log(`ERROR. The number of classes can't be smaller than 3.`)
+            return `ERROR. The number of classes can't be smaller than 3.`
         } else {
             this.numberOfClasses = number;
-            return console.log(this.numberOfClasses)
+            return this.numberOfClasses
+        }
+    }
+};
+const htmlBasics = new Subject ("Hypertext Markup Language - BASIC", 10, false, qinshift, []);
+const htmlAdvanceed = new Subject ("Hypertext Markup Language - ADVANCED", 10, true, qinshift, []);
+const cssBasics = new Subject ("Cascading Style Sheets - BASIC", 11, false, qinshift, []);
+const cssAdvanced = new Subject ("Cascading Style Sheets - ADVANCED", 11, true, qinshift, []);
+qinshift.subjects.push(htmlBasics, htmlAdvanceed, cssBasics)
+console.log(htmlBasics)
+console.log(htmlBasics.overrideClasses(5))
+console.log(htmlBasics.overrideClasses(2))
+console.log(htmlBasics.validatePrice(10))
+console.log(htmlBasics.validatePrice(-10))
+console.log(qinshift.subjects)
+
+function Person(email, phone){
+    this.id = new Date()
+    this.email = email
+    this.phone = phone
+
+    this.validateEmail = function(theEmail, name){
+        if (!theEmail.includes('@')){
+            this.email = ``
+            return `ERROR: Please enter valid email address!!`
+        } else {
+            return `The email address of ${name} is ${theEmail}.`
         }
     }
 }
+function Student(firstName, lastName, age, completedSubjects, academy, subject, email, phone){
+    Object.setPrototypeOf(this, new Person(email, phone))
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.completedSubjects = completedSubjects
+    this.academy = academy
+    this.startAcademy = this.academy
 
-const academy = {
-    name: ``,
-    students: [],
-    subjects: [],
-    start: ``,
-    end: ``,
-    
-    numberOfClasses: function() {
-        return this.subjects.length;
-    },
-    printStudents: function(){
-        console.log(this.students);
-    },
-    printSubjects: function(){
-        console.log(this.subjects);
-    }
-}
-
-const academyOne = Object.create(academy);
-academyOne.name = "Academy One";
-academyOne.subjects = []
-academyOne.start = "02.10.2024"
-academyOne.end = "02.10.2025"
-academyOne.id = `academyOne`
-
-const academyTwo = Object.create(academy);
-academyTwo.name = "Academy Two";
-academyTwo.subjects = []
-academyTwo.start = "02.10.2024"
-academyTwo.end = "02.10.2025"
-academyTwo.id = `academyTwo`
-
-
-const academyOneSubjectOne = Object.create(subject);
-academyOneSubjectOne.title = "Academy One - Subject One";
-academyOneSubjectOne.isElectable = true;
-academyOneSubjectOne.academy = academyOne;
-academyOneSubjectOne.students = []
-academyOne.subjects.push(academyOneSubjectOne);
-
-const academyOneSubjectTwo = Object.create(subject);
-academyOneSubjectTwo.title = "Academy One - Subject Two";
-academyOneSubjectTwo.isElectable = false;
-academyOneSubjectTwo.academy = academyOne;
-academyOneSubjectTwo.students = []
-academyOne.subjects.push(academyOneSubjectTwo);
-
-const academyOneSubjectThree = Object.create(subject);
-academyOneSubjectThree.title = "Academy One - Subject Three";
-academyOneSubjectThree.isElectable = true;
-academyOneSubjectThree.academy = academyOne;
-academyOneSubjectThree.students = []
-academyOne.subjects.push(academyOneSubjectThree);
-
-
-const academyTwoSubjectOne = Object.create(subject);
-academyTwoSubjectOne.title = "Academy Two - Subject One";
-academyTwoSubjectOne.isElectable = true;
-academyTwoSubjectOne.academy = academyTwo;
-academyTwoSubjectOne.students = []
-academyTwo.subjects.push(academyTwoSubjectOne);
-
-const academyTwoSubjectTwo = Object.create(subject);
-academyTwoSubjectTwo.title = "Academy Two - Subject Two";
-academyTwoSubjectTwo.isElectable = false;
-academyTwoSubjectTwo.academy = academyTwo;
-academyTwoSubjectTwo.students = []
-academyTwo.subjects.push(academyTwoSubjectTwo);
-
-const academyTwoSubjectThree = Object.create(subject);
-academyTwoSubjectThree.title = "Academy Two - Subject Three";
-academyTwoSubjectThree.isElectable = true;
-academyTwoSubjectThree.academy = academyTwo;
-academyTwoSubjectThree.students = []
-academyTwo.subjects.push(academyTwoSubjectThree);
-
-const studentOne = Object.create(student);
-studentOne.firstName = "studentOne";
-studentOne.lastName = "oneStudent";
-studentOne.age = 20;
-studentOne.academy = academyOne;
-studentOne.currrentSubject = academyOneSubjectOne;
-studentOne.completedSubjects = [academyOneSubjectTwo];
-academyOneSubjectTwo.students.push(studentOne)
-academyOneSubjectOne.students.push(studentOne)
-allStudents.push(studentOne)
-
-
-const studentTwo = Object.create(student);
-studentTwo.firstName = "studentTwo";
-studentTwo.lastName = "twoStudent";
-studentTwo.age = 22;
-studentTwo.academy = academyOne;
-studentTwo.currrentSubject = academyOneSubjectOne;
-studentTwo.completedSubjects = [academyOneSubjectThree];
-academyOneSubjectThree.students.push(studentTwo)
-academyOneSubjectOne.students.push(studentTwo)
-allStudents.push(studentTwo)
-
-function printAcademyStudents(academyStudents) {
-    console.log(academyStudents);
-    for (i=0; i<academyStudents.length; i++) {
-        let newStudent = document.createElement('option');
-        newStudent.value = academyStudents[i].firstName
-        newStudent.innerText = academyStudents[i].firstName
-
-        document.getElementById(`selectStudent`).appendChild(newStudent);
-    }
-}
-
-
-let academyStudents = []
-let selectAcademy = document.getElementById("selectAcademy");
-selectAcademy.addEventListener("change", function(event){
-    event.preventDefault();
-    document.getElementById("selectStudent").innerHTML = ``
-    academyStudents = allStudents.filter((element) => {
-        if (element.academy.id === selectAcademy.value) {
-            return element
+    this.startSubject = function(){
+        if ((this.academy != undefined)&&(this.academy.subjects.includes(subject))) {
+            this.currentSubject = subject
         }
-    });
-    printAcademyStudents(academyStudents);
-})
+    }
+};
+
+const studentOne = new Student (`Bob`, `Bobsky`, 20, [], qinshift, htmlBasics, `bob@domain.com`, 55500076)
+htmlBasics.students.push(studentOne)
+const studentTwo = new Student (`Tom`, `Tomsky`, 20, [], qinshift, htmlAdvanceed, `tomdomain.com`, 55500076)
+htmlAdvanceed.students.push(studentOne)
+const studentThree = new Student (`Jim`, `Jimsky`, 20, [], qinshift, cssBasics, `jim@domain.com`, 55500076)
+cssBasics.students.push(studentOne)
+const studentFour = new Student (`Paul`, `Paulsky`, 20, [], qinshift, cssAdvanced, `paul@domain.com`, 55500076)
+
+qinshift.students.push(studentOne, studentTwo, studentThree, studentFour)
+
+console.log(studentOne.validateEmail(studentOne.email, studentOne.firstName))
+console.log(studentOne)
+console.log(studentTwo.validateEmail(studentTwo.email, studentTwo.firstName))
+console.log(studentTwo)
+console.log(studentThree.startSubject())
+console.log(studentThree)
+console.log(studentFour.startSubject())
+console.log(studentFour)
 
 
+console.log(htmlBasics)
+console.log(htmlAdvanceed)
+console.log(cssBasics)
+console.log(qinshift)
 
 
 
