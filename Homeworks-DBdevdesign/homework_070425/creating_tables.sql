31e52dad289619dce819dd62e8f95baa738d2a42
StevanRufceski <stevan.rufceski@gmail.com>
2025-04-09 13:27:53 +0200

    added DBhomeworks folder

---
+++ Homeworks-DBdevdesign/homework_070425/creating_tables.sql
@@ -0,0 +1,48 @@
+CREATE TABLE student (
+    id SERIAL PRIMARY KEY,
+    FirstName VARCHAR(30),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    EnrolledDate DATE,
+    Gender VARCHAR(6),
+    NationalIdNumber INTEGER,
+	StudentCardNumber VARCHAR(50),
+);
+CREATE TABLE teacher (
+    id SERIAL PRIMARY KEY,
+    FirstName VARCHAR(30),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    AcademicRank VARCHAR(30),
+    HireDate DATE,
+);
+CREATE TABLE grade_details (
+    id SERIAL PRIMARY KEY,
+    GradeId VARCHAR(1),
+    AchievementTypeId VARCHAR(20),
+    AchievementPoints SMALLINT,
+    AchievementMaxPoints SMALLINT,
+    AchievementMaxDate DATE,
+);
+CREATE TABLE course (
+    id SERIAL PRIMARY KEY,
+    Name VARCHAR(100),
+    Credit SMALLINT,
+    AcademicYear DATE,
+    Semester SMALLINT,
+);
+CREATE TABLE grade (
+    id SERIAL PRIMARY KEY,
+    StudentId INTEGER,
+    CourseId INTEGER,
+    TeacherId INTEGER,
+    Grade SMALLINT,
+	Comment TEXT,
+	CreatedDate DATE,
+);
+CREATE TABLE achievement_type (
+    id SERIAL PRIMARY KEY,
+    Name ARCHAR(50),
+    Description TEXT,
+    ParticipationRate SMALLINT,
+);
