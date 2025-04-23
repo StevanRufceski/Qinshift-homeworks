9dff7e8ea982001e6a7438d25dd3868032b274fe
StevanRufceski <stevan.rufceski@gmail.com>
2025-04-23 13:27:36 +0200

    updateMRA

--- Project-MRA/mlContact/contactPage.js
+++ Project-MRA/mlContact/contactPage.js
@@ -1,37 +1,99 @@
-var language = 'france'
-const content = {
-  en: {
-      title: "Welcome!",
-      message: "This is a simple language selector example."
-  },
-  es: {
-      title: "¡Bienvenido!",
-      message: "Este es un ejemplo simple de selector de idioma."
-  },
-  fr: {
-      title: "Bienvenue!",
-      message: "Ceci est un exemple simple de sélecteur de langue."
-  },
-  de: {
-      title: "Willkommen!",
-      message: "Dies ist ein einfaches Beispiel für einen Sprachwähler."
-  }
-};
+// language selector
+// switching among languages in localStorage
+document.addEventListener('DOMContentLoaded', function () {
+    if (!localStorage.getItem('language')) {
+        localStorage.setItem('language', 'Deutsch');
+    }
+    document.querySelectorAll('#language-selector a img').forEach(option => {
+        if (option.alt == localStorage.getItem('language')) {
+            option.classList.add('langSelection')
+        }
+    })
 
-document.querySelectorAll('.dropdown-content a').forEach(item => {
-  item.addEventListener('click', (e) => {
-      const language = e.target.getAttribute('data-lang');
-      document.querySelector('#content h1').textContent = content[language].title;
-      document.querySelector('#content p').textContent = content[language].message;
-  });
+    const languageOptions = document.querySelectorAll('#language-selector a');
+    const selectedLanguage = document.getElementsByClassName('langSelection')[0];
+
+    languageOptions.forEach(option => {
+        option.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const flagImg = this.querySelector('img');
+            const flagSrc = flagImg.src;
+            const flagAlt = flagImg.alt;
+
+            this.querySelector('img').src = selectedLanguage.src;
+            this.querySelector('img').alt = selectedLanguage.alt;
+
+            selectedLanguage.src = flagSrc;
+            selectedLanguage.alt = flagAlt;
+            localStorage.setItem('language', flagAlt);
+            console.log(flagAlt);
+
+        });
+    });
 });
-if (language === 'english') {
-    console.log(content.en.title)
-} else if (language === 'german') {
-    console.log(content.de.title)
-} else if (language === 'spanish') {
-    console.log(content.es.title)
-} else if (language === 'france') {
-    console.log(content.fr.title)
+// fetching translaed content fromm JSON
+function fetchPeople( ) {
+    fetch("C:/Users/Viktor/Desktop/Tenderi/WEB/GIT/Qinshift-homeworks/Project-MRA/mlContact/contactPage.html")
+    .then(function(response){
+        // RAW RESPONSE CONTAINING METEDATA OF THE REQUEST
+        console.log(response) 
+
+        let jsonResult = response.text() // WILL RETURN STRINGIFIED VALUE OF THE RESPONSE
+        console.log(jsonResult)
+
+        return jsonResult
+    })
+    .then(function(result) {
+        // THIS IS THE JSON RESULT
+        // console.log(result);
+
+        let parsedResult = JSON.parse(result);
+        console.log(parsedResult);
+
+        let swapiPeople = parsedResult.results;
+        console.log(swapiPeople)
+
+        fetchDivResult.innerHTML = ''
+        // FUNCTION TO PRINT RESULT SHOULD BE HERE
+        for(let i = 0; i < swapiPeople.length; i++){
+            fetchDivResult.innerHTML += `<p>${swapiPeople[i].name}</p>`
+        }
+    }) 
+    .catch(function(error){
+        // IF REQUEST WAS UNSUCCESS WE GONNA ENTER THE CATCH
+        console.error('Error happened:', error)
+    })
+    .finally(function(){
+        // NO MATTER IF THE REQUEST IS SUCCESS OR NOT 
+        // FINALLY WILL EXECUTE
+        console.log('IN FINALLY')
+    })
 }
+fetchPeople()
+
+
+
+
+
+
+// const content = {
+//   en: {
+//       title: "Welcome!",
+//       message: "This is a simple language selector example."
+//   },
+//   es: {
+//       title: "¡Bienvenido!",
+//       message: "Este es un ejemplo simple de selector de idioma."
+//   },
+//   fr: {
+//       title: "Bienvenue!",
+//       message: "Ceci est un exemple simple de sélecteur de langue."
+//   },
+//   de: {
+//       title: "Willkommen!",
+//       message: "Dies ist ein einfaches Beispiel für einen Sprachwähler."
+//   }
+// };
+
 
