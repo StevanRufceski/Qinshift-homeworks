09dd72a68f7297aa2b1f9574d549f942558c96b5
Stevan Rufceski <stevan.rufceski@gmail.com>
2025-05-05 19:05:49 +0200

    wip

---
+++ Homeworks-TS-NestJS/Homework-Class-03/nestjsintro/src/books/books.controller.ts
@@ -0,0 +1,79 @@
+import { Controller, Get, Param, Post, Body, Put, Patch, Delete, Query } from '@nestjs/common';
+
+interface Book {
+    id: number;
+    title: string;
+    price: number;
+    author: string;
+}
+
+type UpsertBook = Omit<Book, 'id'>;
+
+interface searchBooksParams {
+    minPrice: string;
+    author: string;
+}
+
+@Controller('books')
+export class BooksController {
+    private books: Book[] = [
+        { id: 1, title: 'Zoki Poki', price: 10, author: 'Olivera Nikolova' },
+    ];
+
+    @Get()
+    getAllBooks(): Book[]{
+        return this.books;
+    }
+
+    @Get('/:id')
+    getBookById(@Param('id') id: string): Book | undefined{
+        return this.books.find((book) => book.id === Number(id));
+    }
+
+    @Post()
+    createBook(@Body() bodyBook: UpsertBook) {
+        const newBook = {
+            ... bodyBook,
+            id: Date.now(),
+        } satisfies Book;
+        this.books.push(newBook);
+        return newBook;
+    }
+    @Put('/:id')
+    changeBook(@Param('id') id: string, @Body() changeBookData: UpsertBook){
+        const bookIndex = this.books.findIndex((book) => book.id = Number(id));
+        const changedBook = {
+            ... changeBookData,
+            id: Number(id),
+        } satisfies Book;
+        this.books[bookIndex] = changedBook;
+        return changedBook
+    }
+    @Patch('/:id')
+    updateBook(@Param('id') id:string, @Body() updateBookData: Partial<UpsertBook>){
+        const bookIndex = this.books.findIndex((book) => book.id = Number(id));
+        const updatedBook = {
+            ...this.books[bookIndex],
+            ...updateBookData,
+            id: Number(id),
+        } satisfies Book;
+        this.books[bookIndex] = updatedBook;
+        return updatedBook
+    }
+    @Delete('/:id')
+    deleteBook(@Param('id') id:string){
+        this.books = this.books.filter((book) => book.id !== Number(id));
+        return this.books;
+    }
+    @Get('/filter')
+    filterBooks(@Query() searchParams: searchBooksParams): Book[] {
+        let filteredBooks = [...this.books];
+        if (searchParams.minPrice){
+            filteredBooks = filteredBooks.filter((book) => book.price > Number(searchParams.minPrice),);
+        }
+        if (searchParams.author){
+            filteredBooks = filteredBooks.filter((book) => book.author === searchParams.author,);
+        }
+        return filteredBooks;
+    }
+}
