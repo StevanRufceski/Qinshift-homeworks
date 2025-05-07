0aa62c4d92720790af998b23597e08a5be340e8a
StevanRufceski <stevan.rufceski@gmail.com>
2025-05-07 10:15:50 +0200

    homework class03 imporovement

--- Homeworks-TS-NestJS/Homework-Class-03/nestjsintro/src/books/books.controller.ts
+++ Homeworks-TS-NestJS/Homework-Class-03/nestjsintro/src/books/books.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Param, Post, Body, Put, Patch, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Patch, Delete, Query, HttpCode, HttpStatus, NotFoundException } from '@nestjs/common';
+import { error } from 'console';
+import { NotFoundError } from 'rxjs';
 
 interface Book {
     id: number;
@@ -10,7 +12,7 @@ interface Book {
 type UpsertBook = Omit<Book, 'id'>;
 
 interface searchBooksParams {
-    minPrice: string;
+    minPrice: number;
     author: string;
 }
 
@@ -19,30 +21,43 @@ export class BooksController {
     private books: Book[] = [
         { id: 1, title: 'Zoki Poki', price: 10, author: 'Olivera Nikolova' },
     ];
-    @Get('/filter')
+    @Get()
+    @HttpCode(HttpStatus.FOUND)
     filterBooks(@Query() searchParams: searchBooksParams): Book[] {
         let filteredBooks = [...this.books];
-        if (searchParams.minPrice){
-            filteredBooks = filteredBooks.filter((book) => book.price > Number(searchParams.minPrice),);
+        if (!searchParams){
+            return this.books;
+        } else {
+            if (searchParams.minPrice){
+                filteredBooks = filteredBooks.filter((book) => book.price > Number(searchParams.minPrice),);
+            }
+            if (searchParams.author){
+                filteredBooks = filteredBooks.filter((book) => book.author === searchParams.author,);
+            }
+            return filteredBooks;
         }
-        if (searchParams.author){
-            filteredBooks = filteredBooks.filter((book) => book.author === searchParams.author,);
-        }
-        return filteredBooks;
-    }
-
-    @Get()
-    getAllBooks(): Book[]{
-        return this.books;
     }
 
     @Get('/:id')
+    @HttpCode(HttpStatus.FOUND)
     getBookById(@Param('id') id: string): Book | undefined{
         return this.books.find((book) => book.id === Number(id));
     }
 
     @Post()
+    @HttpCode(HttpStatus.CREATED)
     createBook(@Body() bodyBook: UpsertBook) {
+        if (!bodyBook.title || bodyBook.title.trim().length === 0) {
+            throw new Error('Title is required');
+        }
+        
+        if (!bodyBook.author || bodyBook.author.trim().length === 0) {
+            throw new Error('Author is required');
+        }
+    
+        if (typeof bodyBook.price !== 'number' || bodyBook.price <= 0) {
+            throw new Error('Price must be a positive number');
+        }
         const newBook = {
             ... bodyBook,
             id: Date.now(),
@@ -51,8 +66,23 @@ export class BooksController {
         return newBook;
     }
     @Put('/:id')
+    @HttpCode(HttpStatus.ACCEPTED)
     changeBook(@Body() changeBookData: UpsertBook, @Param('id') id: string){
         const bookIndex = this.books.findIndex((book) => book.id === Number(id));
+        if (bookIndex === -1) {
+            throw new NotFoundException(`Book with ID ${id} not found`);
+        }
+        if (!changeBookData.title || changeBookData.title.trim().length === 0) {
+            throw new Error('Title is required');
+        }
+        
+        if (!changeBookData.author || changeBookData.author.trim().length === 0) {
+            throw new Error('Author is required');
+        }
+    
+        if (typeof changeBookData.price !== 'number' || changeBookData.price <= 0) {
+            throw new Error('Price must be a positive number');
+        }
         const changedBook = {
             ... changeBookData,
             id: Number(id),
@@ -61,8 +91,24 @@ export class BooksController {
         return changedBook
     }
     @Patch('/:id')
+    @HttpCode(HttpStatus.ACCEPTED)
     updateBook(@Body() updateBookData: Partial<UpsertBook>, @Param('id') id:string){
         const bookIndex = this.books.findIndex((book) => book.id === Number(id));
+        if (bookIndex === -1) {
+            throw new NotFoundException(`Book with ID ${id} not found`);
+        }
+        if (updateBookData.title && updateBookData.title.trim().length === 0) {
+            throw new Error('Title is required');
+        }
+        
+        if (updateBookData.author && updateBookData.author.trim().length === 0) {
+            throw new Error('Author is required');
+        }
+        if (updateBookData.price !== undefined) {
+            if ((updateBookData.author && typeof updateBookData.price !== 'number') || (updateBookData.author && updateBookData.price <= 0)) {
+                throw new Error('Price must be a positive number');
+            }
+        }
         const updatedBook = {
             ...this.books[bookIndex],
             ...updateBookData,
@@ -72,7 +118,12 @@ export class BooksController {
         return updatedBook
     }
     @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
     deleteBook(@Param('id') id:string){
+        const bookIndex = this.books.findIndex((book) => book.id === Number(id))
+        if (bookIndex === -1) {
+            throw new NotFoundException(`Book with ID ${id} not found`);
+        }
         this.books = this.books.filter((book) => book.id !== Number(id));
         return this.books;
     }
