7bd192f24cc17fdf6424d6f9cd22ea4f52eebdef
StevanRufceski <stevan.rufceski@gmail.com>
2025-07-21 13:41:20 +0200

    hw05 update

--- Homeworks-React/Homework-class05/e-commerce-app/src/components/ProductCard.tsx
+++ Homeworks-React/Homework-class05/e-commerce-app/src/components/ProductCard.tsx
@@ -10,13 +10,14 @@ export const ProductCard = (props: ProductCardProps) => {
         <div className="product-card">
             <span className="product-category">{props.product.category}</span>
             <img src={props.product.image} alt={props.product.title} className="product-image" />
-            <div className="product-content">
-                <h3 className="product-title">{props.product.title}</h3>
-                <p className="product-description">{props.product.description}</p>
+            <div>
+                <div className="product-content">
+                    <h3 className="product-title">{props.product.title}</h3>
+                    <p className="product-description">{props.product.description}</p>
+                </div>
                 <div className="product-details">
                     <p className="product-rating">⭐ {props.product.rating.rate} ({props.product.rating.count} reviews)</p>
                     <p className="product-price">💰 ${props.product.price}</p>
-                    
                 </div>
             </div>
         </div>
